c54dd783b174d58143cccc7009a0a942af2a9e2b
delowarshizar <husain22205101453@diu.edu.bd>
2025-12-12 01:30:40 +0600

    post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const Post = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Post = ({ post }) => {
+  const {title} = post;
+  return (
+    <div style={{border: '1px solid red',margin:"10px", padding:"10px"}}>
+      <h1>
+        Title: {title}
+      </h1>
+    </div>
+  );
 };
 
 export default Post;
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { useLoaderData } from "react-router";
-
+import Post from "../Post/Post";
 const Posts = () => {
   const posts = useLoaderData();
   console.log(posts);
   return (
     <div>
       <h1>Total number of post is {posts.length}</h1>
-      
+      {posts.map((post) => (
+        <Post key={post.id} post={post}></Post>
+      ))}
     </div>
   );
 };
