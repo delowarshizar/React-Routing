7d162f57ab2d60b3b21556cbe03e486c9b1ecec4
delowarshizar <husain22205101453@diu.edu.bd>
2025-12-12 02:11:41 +0600

    update

--- src/components/PostDetail/PostDetail.jsx
+++ src/components/PostDetail/PostDetail.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useLoaderData } from 'react-router';
+import React from "react";
+import { useLoaderData } from "react-router";
 
 const PostDetail = () => {
-    const Post = useLoaderData();
-    return (
-        <div>
-            <h1>title: {Post.title}</h1>
-            <p> Detail {Post.body}</p>
-        </div>
-    );
+  const Post = useLoaderData();
+  return (
+    <div>
+      <h1>title: {Post.title}</h1>
+      <p> Detail: {Post.body}</p>
+    </div>
+  );
 };
 
 export default PostDetail;
