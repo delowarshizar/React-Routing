ae16fcc7a90475c5b60b93e26a4443a988416c3e
delowarshizar <husain22205101453@diu.edu.bd>
2025-12-18 21:09:15 +0600

    loading using navigation

--- src/components/root/Root.jsx
+++ src/components/root/Root.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import Header from '../header/Header';
-import { Outlet } from 'react-router';
-import Footer from '../footer/Footer';
+import React from "react";
+import Header from "../header/Header";
+import { Outlet, useNavigation } from "react-router";
+import Footer from "../footer/Footer";
 
 const Root = () => {
-    return (
-        <div>
-            <Header></Header>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  const navigation = useNavigation();
+  const isNavigating = Boolean(navigation.location);
+  return (
+    <div>
+      <Header></Header>
+      {isNavigating && <h1>Loading....</h1>}
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
