a764b41918954cde552701f37174a4e5c7e53e2c
delowarshizar <husain22205101453@diu.edu.bd>
2025-12-18 21:01:16 +0600

    useNavigate for back pageg

--- src/components/PostDetail/PostDetail.jsx
+++ src/components/PostDetail/PostDetail.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router";
 import { useLoaderData } from "react-router";
-
 const PostDetail = () => {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate(-1);
+  };
   const Post = useLoaderData();
   return (
     <div>
       <h1>title: {Post.title}</h1>
       <p> Detail: {Post.body}</p>
+      <button onClick={handleNavigate}>Go Back</button>
     </div>
   );
 };
