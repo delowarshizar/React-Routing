7b8dd49054e9486c1ab3674b62629bf1d5a467fe
delowarshizar <husain22205101453@diu.edu.bd>
2025-12-18 20:46:22 +0600

    useNavigate for dynamic routing

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 const Post = ({ post }) => {
-  const {title,id} = post;
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate(`/posts/${id}`);
+  };
+  const { title, id } = post;
   return (
-    <div style={{border: '1px solid red',margin:"10px", padding:"10px"}}>
-      <h1>
-        Title: {title}
-      </h1>
-     <Link to={`/posts/${id}`}><button>See more</button></Link>
+    <div style={{ border: "1px solid red", margin: "10px", padding: "10px" }}>
+      <h1>Title: {title}</h1>
+      <Link to={`/posts/${id}`}>
+        <button>See more</button>
+      </Link>
+      <button onClick={handleNavigate}>see detail - {id}</button>
     </div>
   );
 };
